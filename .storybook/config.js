24eab32a71a9bccaaa198874d737d82967fe0159
peatroot <garethpeat@gmail.com>
2019-04-09 10:51:59 +0100

    Configure storybook with distinct .babelrc and add Link

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
+
+// decorate
+addDecorator(StoryRouter());
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../lib/components', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
