1670c677879c99c3bdaf008c16abac8c8f7d193e
peatroot <garethpeat@gmail.com>
2019-04-09 14:00:40 +0100

    Add icons to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,13 @@
 import { configure, addDecorator } from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
 import { muiTheme } from 'storybook-addon-material-ui';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
+import setupIcons from '../lib/icons/setupIcons';
 import { theme } from '../lib/theme';
+
+// load icons
+setupIcons();
 
 // decorate all stories with the following
 addDecorator(StoryRouter());
